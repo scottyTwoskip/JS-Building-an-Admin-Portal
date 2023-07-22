e8b6cdbbdece7801f078bd0508aa6914662f8b8b
scottyTwoskip <scottyeansor@gmail.com>
2023-07-21 18:30:01 -0700

    seth, sasa and me could not figure this out.

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,18 @@
-
 // Your Code Here
+//retrieve a list of books 
+//display the list of books on the admin
+function main(){
+let response = await fetch('http://localhost:3001/listBooks', {
+    method: "GET",
+    headers: {
+        'Content-Type' : 'application/json'
+    },
+    body: JSON.stringify({
+        "id" : 3,
+        "title": "The Annals of Arathrae",
+    })
+})
+}
+let updatedBook = await response.json()
+console.log(updatedBook)
+
